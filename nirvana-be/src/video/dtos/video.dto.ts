f512cc47a0cac9ee63c3383432bbeb071f7f9b97
Servando Bermudez <servando.bermudez24@gmail.com>
2023-09-29 20:52:15 -0300

    Fixed Backend URL validation

--- nirvana-be/src/video/dtos/video.dto.ts
+++ nirvana-be/src/video/dtos/video.dto.ts
@@ -1,10 +1,18 @@
-import { IsOptional, IsString, IsUrl } from 'class-validator';
+import { IsOptional, IsString, IsUrl, Matches } from 'class-validator';
 
 export class CreateVideoDto {
   @IsString()
   name: string;
 
   @IsUrl()
+  @Matches(
+    new RegExp(
+      `(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(.*?((?=[&#?])|$))`,
+    ),
+    {
+      message: 'Invalid Youtube url',
+    },
+  )
   url: string;
 }
 
