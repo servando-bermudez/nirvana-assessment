05b2b0d510ffa0880f74c1697c672461bf7d224b
Servando Bermudez <servando.bermudez24@gmail.com>
2023-09-29 21:05:02 -0300

    Fixed Frontend URL Youtube Validation

--- nirvana-fe/src/components/VideoForm/VideoForm.tsx
+++ nirvana-fe/src/components/VideoForm/VideoForm.tsx
@@ -46,11 +46,19 @@ const VideoForm: React.FC<VideoFormProps> = ({ visible, setVisibility, onCreate 
         </Form.Item>
 
         <Form.Item<FieldType>
-          label="URL"
+          label="Youtube URL"
           name="url"
-          rules={[{ required: true }, { type: 'url' }, { type: 'string', min: 6 }]}
+          rules={[
+            {
+              required: true,
+              pattern: new RegExp(`(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(.*?((?=[&#?])|$))`), /* eslint-disable-line */
+              message: 'Invalid Youtube URL'
+            },
+            { type: 'url', message: '' },
+            { type: 'string', message: '' }
+          ]}
         >
-          <Input placeholder="Video URL" />
+          <Input placeholder="https://www.youtube.com/watch?v=..." />
         </Form.Item>
       </Form>
     </Modal>
